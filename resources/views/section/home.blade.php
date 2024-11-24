ab4241860b1eb744fa43028541064c9522999264
Angel Kyle Alaba <AngelKyleAlaba@gmail.com>
2024-11-24 20:33:53 +0800

    .

--- resources/views/section/home.blade.php
+++ resources/views/section/home.blade.php
@@ -14,31 +14,31 @@
         <div class="text-md text-gray-600 font-medium mb-4 ml-40 mt-9">Inventory Summary</div>
 
         <div class="flex w-full pl-28 pr-44 justify-evenly">
-            <div class=" bg-slate-400 bg-opacity-15 flex justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm ">
-                <img src="{{asset('imgs/item.png')}}" alt="Quantity" class="w-8 h-8">
+            <div class=" bg-slate-400 bg-opacity-15 flex flex-col items-center justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm ">
+                <img src="{{asset('imgs/item.png')}}" alt="Quantity" class="w-9 h-9 mb-5">
                 <p class=" text-3xl font-bold">{{ $itemCount }}</p>
-                <p>Items</p>
+                <p class="text-gray-600 font-medium">Items</p>
 
             </div>
 
-            <div class=" bg-slate-400 bg-opacity-15 flex justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm">
-                <img src="{{asset('imgs/category.png')}}" alt="Quantity" class="w-8 h-8">
+            <div class=" bg-slate-400 bg-opacity-15 flex flex-col items-center justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm">
+                <img src="{{asset('imgs/category.png')}}" alt="Quantity" class="w-10 h-11 mb-5">
                 <p class=" text-3xl font-bold">{{ $categoryCount }}</p>
-                <p>Categories</p>
+                <p class="text-gray-600 font-medium mb-2">Categories</p>
             </div>
 
 
-            <div class=" bg-slate-400 bg-opacity-15 flex justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm">
-                <img src="{{asset('imgs/quantity.png')}}" alt="Quantity" class="w-8 h-8">
+            <div class=" bg-slate-400 bg-opacity-15 flex flex-col items-center justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm">
+                <img src="{{asset('imgs/quantity.png')}}" alt="Quantity" class="w-9 h-9 mb-6">
                 <p class=" text-3xl font-bold">{{ $totalQuantity }}</p>
-                <p>Quantity</p>
+                <p class="text-gray-600 font-medium">Quantity</p>
             </div>
 
 
-            <div class=" bg-slate-400 bg-opacity-15 flex justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm">
-                <img src="{{asset('imgs/value.png')}}" alt="Quantity" class="w-8 h-8">
-                <p class=" text-3xl font-bold">₱{{ $totalValue }}</p>
-                <p>Tolal Value</p>
+            <div class=" bg-slate-400 bg-opacity-15 flex flex-col items-center justify-center w-44 h-40 border-r-2 border-r-slate-300 rounded-md shadow-slate-400 shadow-sm">
+                <img src="{{asset('imgs/value.png')}}" alt="Quantity" class="w-9 h-9 mb-6">
+                <p class=" text-xl font-bold ">₱{{ $totalValue }}</p>
+                <p class="text-gray-600 font-medium">Total Value</p>
             </div>
         </div>
     </div>
