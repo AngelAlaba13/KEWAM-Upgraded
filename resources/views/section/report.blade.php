c4d02cd4c93a58399232b2c5804140c665775907
Angel Kyle Alaba <AngelKyleAlaba@gmail.com>
2024-12-01 15:19:20 +0800

    report page na gyud

--- resources/views/section/report.blade.php
+++ resources/views/section/report.blade.php
@@ -3,6 +3,36 @@
 <div class="flex flex-col h-screen">
     <div class="bg-slate-200 flex-1 ml-16 ">
         <p class="text-xl text-gray-500 ml-5 mt-5">Items</p>
+
+        <div>
+            <canvas id="myChart"></canvas>
+          </div>
+
+          <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+          <script>
+            const ctx = document.getElementById('myChart');
+
+            new Chart(ctx, {
+              type: 'bar',
+              data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                  label: '# of Votes',
+                  data: [12, 19, 3, 5, 2, 3],
+                  borderWidth: 1
+                }]
+              },
+              options: {
+                scales: {
+                  y: {
+                    beginAtZero: true
+                  }
+                }
+              }
+            });
+          </script>
+
     </div>
 
     <div class="bg-zinc-200 flex-1 ml-16 text-xl text-gray-500">
