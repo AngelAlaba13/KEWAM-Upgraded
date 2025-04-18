2ce34b0227a472726284831a632038a8c1ad8df0
AngelAlaba13 <angelkylealaba@gmail.com>
2025-04-18 21:36:32 +0800

    registration form draft

---
+++ resources/views/section/registration.blade.php
@@ -0,0 +1,30 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<form action="{{ route('section.register') }}" method="POST">
+    @csrf
+
+    <div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" required autofocus>
+    </div>
+
+    <div>
+        <label for="role">Role</label>
+        <input type="text" name="role" id="role" required>
+    </div>
+
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required>
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="passsword" id="password" required>
+    </div>
+
+    <div>
+        <label for="conPassword">Confirm Password</label>
+        <input type="conpassword" name="conpasssword" id="conpassword" required>
+    </div>
+</form>
